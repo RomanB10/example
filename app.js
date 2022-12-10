a19ed9f8acb7c0777d21811386a4891103c0ad0f
Roman B <onwork.r@yandex.ru>
2022-12-10 22:24:51 +0300

    B

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 // создаем сервер
 const app = express();
 
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', (err) => {
+  if (err) throw err;
+  console.log('Connected to MongoDB!!!');
+});
 
 
 // Слушаем 3000 порт
